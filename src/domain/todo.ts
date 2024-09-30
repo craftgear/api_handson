eeafe2afc193ec8d6d7ec7f1f10a35ae474df101
Shunsuke Watanabe <craftgear@gmail.com>
2024-09-30 21:25:09 +0900

    chapter 03

--- src/domain/todo.ts
+++ src/domain/todo.ts
@@ -2,9 +2,17 @@ import { z } from 'zod';
 
 const TodoSchema = z.object({
   id: z.string().brand('TodoId'),
-  title: z.string(),
+  title: z
+    .string({
+      required_error: 'Title is required',
+    })
+    .trim()
+    .min(1, { message: 'Title must be at least 1 character long' }),
   completed: z.boolean().default(false),
 });
 
 export type Todo = z.infer<typeof TodoSchema>;
 export type TodoId = Todo['id'];
+
+export const parseTodo = (data: unknown): Todo => TodoSchema.parse(data);
+export const isComplete = (todo: Todo) => todo.completed;
