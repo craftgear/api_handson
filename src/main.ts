d767a88afe2a870ab751858ddb6628e3e2742aa6
Shunsuke Watanabe <craftgear@gmail.com>
2024-10-05 11:32:05 +0900

    ch08

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { Hono } from "hono";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { swaggerUI } from "@hono/swagger-ui";
 import { HTTPException } from "hono/http-exception";
 
 import { todoRoute } from "./routes/todo";
 
-const app = new Hono();
+const app = new OpenAPIHono();
 app.route("/v1", todoRoute);
 app.onError((e, c) => {
   if (e instanceof HTTPException) {
@@ -11,5 +12,19 @@ app.onError((e, c) => {
   }
   return c.json({ error: e.message }, 500);
 });
+
+app.doc31("/open-api", {
+  openapi: "3.1.0",
+  info: { title: "API Hands On", version: "1" },
+  servers: [{ url: "http://localhost:3000/" }],
+  tags: [
+    {
+      name: "todos",
+      description: "Operations about todos",
+    },
+  ],
+});
+
+app.get("/doc", swaggerUI({ url: "/open-api" }));
 
 export default app;
