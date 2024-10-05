a36e8bd55518bb357b8cf52e2c85964cd46acfb1
Shunsuke Watanabe <craftgear@gmail.com>
2024-10-05 11:00:30 +0900

    ch06

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { todoRoute } from "./routes/todo";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const app = new Hono();
+app.route("/v1", todoRoute);
+app.onError((e, c) => {
+  if (e instanceof HTTPException) {
+    return e.getResponse();
+  }
+  return c.json({ error: e.message }, 500);
+});
+
+export default app;
