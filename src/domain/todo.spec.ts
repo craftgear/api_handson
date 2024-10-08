8b02c6c059c671ca994e1a8b946ee5fcbb08034d
Shunsuke Watanabe <craftgear@gmail.com>
2024-10-08 09:12:48 +0900

    chapter 03

---
+++ src/domain/todo.spec.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect, expectTypeOf } from 'vitest';
+import {
+  parseTodo,
+  parseTodoId,
+  parseNewTodo,
+  TodoId,
+} from './todo';
+
+describe('parseTodo', () => {
+  it('parse a valid todo', () => {
+    expect(parseTodo({ id: 1, title: 'Buy milk' })).toEqual({
+      id: 1,
+      title: 'Buy milk',
+      done: false,
+    });
+    const now = new Date();
+    expect(parseTodo({ id: 1, title: 'Buy milk done', done: true, doneAt: now })).toEqual({
+      id: 1,
+      title: 'Buy milk done',
+      done: true,
+      doneAt: now,
+    });
+  });
+});
+
+describe('parseTodoId', () => {
+  it('parse a valid todo id', () => {
+    const todoId = parseTodoId(1);
+    expectTypeOf(todoId).toEqualTypeOf<TodoId>();
+  });
+});
+
+describe('parseNewTodo', () => {
+  it('parse a valid new todo', () => {
+    expect(parseNewTodo({ title: 'Buy milk' })).toEqual({
+      title: 'Buy milk',
+      done: false,
+    });
+  });
+});
