ee3748c609dd7bb149042e2be67d7270e9893d93
Shunsuke Watanabe <craftgear@gmail.com>
2024-10-08 09:32:51 +0900

    chapter 05

--- src/domain/todo.ts
+++ src/domain/todo.ts
@@ -23,3 +23,10 @@ export type NewTodo = Omit<Todo, 'id'>;
 export const parseNewTodo = (data: unknown): NewTodo => TodoSchema.omit({ id: true }).parse(data);
 
 export const isComplete = (todo: Todo) => todo.done;
+
+export type TodoRepository = {
+  selectAll: () => Promise<Todo[]>;
+  selectById: (id: TodoId) => Promise<Todo | null>;
+  insert: (todo: NewTodo) => Promise<Todo | null>;
+  setCompleted: (id: TodoId) => Promise<Todo | null>;
+};
---
+++ src/usecases/todo.spec.ts
@@ -0,0 +1,44 @@
+import { describe, it, expect, vi } from 'vitest';
+import { readTodos, createTodo, completeTodo } from './todo';
+import type { TodoRepository, TodoId } from '../domain/todo';
+
+describe.concurrent('todo usecases', () => {
+  const repository: TodoRepository = {
+    insert: vi.fn(),
+    selectAll: vi.fn(),
+    selectById: vi
+      .fn()
+      .mockResolvedValueOnce({
+        id: 1 as TodoId,
+        title: 'Buy milk',
+        done: false,
+      })
+      .mockResolvedValueOnce(null),
+    setCompleted: vi.fn(),
+  };
+
+  it('reads todos', async () => {
+    await readTodos(repository);
+    expect(repository.selectAll).toHaveBeenCalled();
+  });
+
+  it('creates a new todo', async () => {
+    const title = 'Buy milk';
+    await createTodo(repository, title);
+    expect(repository.insert).toHaveBeenCalledWith({ title, done: false });
+  });
+
+  it('completes a todo', async () => {
+    const id = 1 as TodoId;
+    await completeTodo(repository, id);
+    expect(repository.selectById).toHaveBeenCalledWith(id);
+    expect(repository.setCompleted).toHaveBeenCalledWith(id);
+  });
+
+  it('throws an error when todo is not found', async () => {
+    const id = 999 as TodoId;
+    expect(() => completeTodo(repository, id)).rejects.toThrowError(
+      'Todo not found'
+    );
+  });
+});
