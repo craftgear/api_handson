05947487f4496ee9456818ad184a4b5d627734cd
Shunsuke Watanabe <craftgear@gmail.com>
2024-10-25 15:13:13 +0900

    clean slate

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,16 @@
-import { serve } from "@hono/node-server";
-import { OpenAPIHono } from '@hono/zod-openapi';
-import { swaggerUI } from '@hono/swagger-ui';
-import { HTTPException } from "hono/http-exception";
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
 
-import { todoRoute } from "./routes/todo";
+const app = new Hono()
 
-const app = new OpenAPIHono();
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
 
-app.route("/v1", todoRoute);
-app.onError((e, c) => {
-  if (e instanceof HTTPException) {
-    return e.getResponse();
-  }
-  return c.json({ error: e.message }, 500);
-});
-
-app.doc31('/open-api', {
-  openapi: '3.1.0',
-  info: { title: 'API Hands On', version: '1' },
-  servers: [{ url: 'http://localhost:3000' }],
-  tags: [
-    {
-      name: 'todos',
-      description: 'Operations about todos',
-    },
-  ],
-});
-app.get('/doc', swaggerUI({ url: '/open-api' }));
-
-const port = 3000;
-console.log(`Server is running on port ${port}`);
+const port = 3000
+console.log(`Server is running on port ${port}`)
 
 serve({
   fetch: app.fetch,
-  port,
-});
+  port
+})
