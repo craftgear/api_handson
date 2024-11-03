03182f218deb497bca4052661a57641c479a8428
Shunsuke Watanabe <craftgear@gmail.com>
2024-11-03 15:19:58 +0900

    ch03

---
+++ src/todo/domain.spec.ts
@@ -0,0 +1,48 @@
+import { describe, it, expect, expectTypeOf } from "vitest";
+import {
+  parseTodo,
+  parseTodoId,
+  parseNewTodo,
+  Todo,
+  TodoId,
+  NewTodo,
+} from "./domain";
+
+describe("parseTodo", () => {
+  it("parse a valid todo", () => {
+    const result = parseTodo({ id: 1, title: "Buy milk" });
+    expect(result).toEqual({
+      id: 1,
+      title: "Buy milk",
+      done: false,
+    });
+    expectTypeOf(result).toEqualTypeOf<Todo>();
+    const now = new Date();
+    expect(
+      parseTodo({ id: 1, title: "Buy milk done", done: true, doneAt: now }),
+    ).toEqual({
+      id: 1,
+      title: "Buy milk done",
+      done: true,
+      doneAt: now,
+    });
+  });
+});
+
+describe("parseTodoId", () => {
+  it("parse a valid todo id", () => {
+    const todoId = parseTodoId(1);
+    expectTypeOf(todoId).toEqualTypeOf<TodoId>();
+  });
+});
+
+describe("parseNewTodo", () => {
+  it("parse a valid new todo", () => {
+    const result = parseNewTodo({ title: "Buy milk" });
+    expect(result).toEqual({
+      title: "Buy milk",
+      done: false,
+    });
+    expectTypeOf(result).toEqualTypeOf<NewTodo>();
+  });
+});
---
+++ src/todo/domain.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const TodoSchema = z.object({
+  id: z.number().brand("TodoId"),
+  title: z.string(),
+  done: z.coerce.boolean().default(false),
+  doneAt: z.coerce.date().nullish(),
+});
+
+export type Todo = z.infer<typeof TodoSchema>;
+export type TodoId = Todo["id"];
+
+export const parseTodo = (data: unknown): Todo => TodoSchema.parse(data);
+export const parseTodoId = (id: number): TodoId =>
+  TodoSchema.shape.id.parse(id);
+
+export type NewTodo = Omit<Todo, "id">;
+export const parseNewTodo = (data: unknown): NewTodo =>
+  TodoSchema.omit({ id: true }).parse(data);
